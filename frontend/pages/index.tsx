a37381cc86d5a697783b6057d3c5156eb6fb26f4
imamura7321 <sh7321ee@gmail.com>
2023-05-25 16:43:42 +0900

    test

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -16,6 +16,7 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <p>Test</p>
 
         <p className={styles.description}>
           Get started by editing{' '}
