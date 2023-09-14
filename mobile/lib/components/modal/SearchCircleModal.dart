d106a528a9d76ff3d38edddea9a51275aa799251
cancan007 <shota.moue0211@gmail.com>
2023-09-14 23:47:08 +0900

    creating SearchCircleModal

---
+++ mobile/lib/components/modal/SearchCircleModal.dart
@@ -0,0 +1,31 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:mobile/components/common/ColumnWithGap.dart';
+import 'package:mobile/components/common/RowWithGap.dart';
+
+class SearchCircleModal extends StatelessWidget {
+  final double gap;
+  final List<Widget> children;
+  final bool? horizontal;
+  const SearchCircleModal({
+    super.key,
+    required this.gap,
+    required this.children,
+    this.horizontal,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialog(
+  //title: const Text('タイトル'),
+  contentPadding: EdgeInsets.all(16),
+  children: <Widget>[
+    ColumnViewWithGap(gap: 16, children: <Widget>[
+      RowViewWithGap(gap: 8, children: <Widget>[
+        Text('場所:',style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),)
+      ])
+    ])
+  ],
+);
+}
