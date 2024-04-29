268ac9ccc4c2239f45dda21e58ed380266db7c63
cancan007 <shota.moue0211@gmail.com>
2024-04-29 11:24:20 +0900

    tried to push

--- mobile/lib/mocks/mocks.dart
+++ mobile/lib/mocks/mocks.dart
@@ -114,3 +114,5 @@ final mockSquare11 = Square(
     photoUrl: 'public/consul.jpeg',
     detail: '各大学のコンサル志望の方々で情報交換をし合うための場所です！さまざまな対面イベントもあるのでぜひ！',
     userJoinedSquares: List.generate(17, (index) => mockUserJoinedSquare));
+
+final testString = 'これはテストです';
